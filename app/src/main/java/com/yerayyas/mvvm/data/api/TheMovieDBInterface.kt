bbaff44187e2e01ab799c05f3fc1874fa9c6098a
yeray-yas <yerayarucas@gmail.com>
2022-04-28 21:39:36 +0200

    add TheMovieDBClient and TheMovieDBInterface classes

---
+++ app/src/main/java/com/yerayyas/mvvm/data/api/TheMovieDBInterface.kt
@@ -0,0 +1,19 @@
+package com.yerayyas.mvvm.data.api
+
+
+
+import com.yerayyas.mvvm.data.vo.MovieResponse
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface TheMovieDBInterface {
+
+
+    @GET("movie/popular")
+    fun getPopularMovie(@Query("page") page: Int): Single<MovieResponse>
+
+    @GET("movie/{movie_id}")
+    fun getData(@Path("movie_id") id :Int): Single<MovieDetails>
+}
--- app/src/main/java/com/yerayyas/mvvm/ui/popular_movie/MainActivity.kt
+++ app/src/main/java/com/yerayyas/mvvm/ui/popular_movie/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.yerayyas.mvvm
+package com.yerayyas.mvvm.ui.popular_movie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yerayyas.mvvm.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
