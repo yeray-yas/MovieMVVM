fff215bcf3f5668c877691cbeb03d37236e642e6
yeray-yas <yerayarucas@gmail.com>
2022-04-28 22:06:48 +0200

    fixing details

--- app/src/main/java/com/yerayyas/mvvm/ui/single_movie_details/SingleActivity.kt
+++ app/src/main/java/com/yerayyas/mvvm/ui/single_movie_details/SingleActivity.kt
@@ -1,37 +1,77 @@
 package com.yerayyas.mvvm.ui.single_movie_details
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.Transformations
-import androidx.paging.LivePagedListBuilder
-import androidx.paging.PagedList
-import com.yerayyas.mvvm.data.api.POST_PER_PAGE
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.yerayyas.mvvm.data.api.POSTER_BASE_URL
+import com.yerayyas.mvvm.data.api.TheMovieDBClient
 import com.yerayyas.mvvm.data.api.TheMovieDBInterface
-import com.yerayyas.mvvm.data.repository.MovieDataSource
-import com.yerayyas.mvvm.data.repository.MovieDataSourceFactory
 import com.yerayyas.mvvm.data.repository.NetworkState
-import com.yerayyas.mvvm.data.vo.Movie
-import io.reactivex.disposables.CompositeDisposable
+import com.yerayyas.mvvm.data.vo.MovieDetails
+import kotlinx.android.synthetic.main.activity_single.*
+import com.yerayyas.mvvm.R
 
-class MoviePagedListRepository (private val apiService : TheMovieDBInterface) {
+class SingleActivity : AppCompatActivity() {
 
-    lateinit var moviePagedList: LiveData<PagedList<Movie>>
-    lateinit var moviesDataSourceFactory: MovieDataSourceFactory
+    private lateinit var viewModel: SingleMovieViewModel
+    private  lateinit var movieRepository: MovieDetailsRepository
 
-    fun fetchLiveMoviePagedList (compositeDisposable: CompositeDisposable) : LiveData<PagedList<Movie>> {
-        moviesDataSourceFactory = MovieDataSourceFactory(apiService, compositeDisposable)
 
-        val config = PagedList.Config.Builder()
-            .setEnablePlaceholders(false)
-            .setPageSize(POST_PER_PAGE)
-            .build()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single)
 
-        moviePagedList = LivePagedListBuilder(moviesDataSourceFactory, config).build()
+        val movieId:Int=intent.getIntExtra("id",1)
+        println("id $movieId")
 
-        return moviePagedList
+        val apiService: TheMovieDBInterface = TheMovieDBClient.getClient()
+        movieRepository= MovieDetailsRepository(apiService)
+
+        viewModel=getViewModel(movieId)
+
+        viewModel.movieDetails.observe(this, Observer {
+            bindUI(it)
+        })
+
+        viewModel.networkState.observe(this, Observer {
+            progress_bar.visibility=if (it== NetworkState.LOADING)View.VISIBLE else View.GONE
+            txt_error.visibility=if (it== NetworkState.ERROR)View.VISIBLE else View.GONE
+        })
+
     }
 
-    fun getNetworkState(): LiveData<NetworkState> {
-        return Transformations.switchMap<MovieDataSource, NetworkState>(
-            moviesDataSourceFactory.moviesLiveDataSource, MovieDataSource::networkState)
+    @SuppressLint("SetTextI18n")
+    private fun bindUI(it: MovieDetails?) {
+        if (it != null) {
+            movie_title.text = it.title
+
+            movie_rating.text = it.rating.toString()
+
+            movie_overview.text = it.overview
+
+
+            val moviePosterURL = POSTER_BASE_URL + it.posterPath
+            Glide.with(this)
+                .load(moviePosterURL)
+                .into(iv_movie_poster)
+        }
+
     }
+
+
+    private fun getViewModel(movieId:Int) : SingleMovieViewModel {
+        return ViewModelProvider(this, object :ViewModelProvider.Factory {
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                @Suppress("UNCHECKED_CAST")
+                return SingleMovieViewModel(movieRepository,movieId) as T
+            }
+
+        })[SingleMovieViewModel::class.java]
+    }
+
 }
