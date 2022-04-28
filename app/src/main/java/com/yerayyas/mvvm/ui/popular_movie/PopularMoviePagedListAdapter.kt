6631e8f3af736dd45850168d41a438ea61f002d5
yeray-yas <yerayarucas@gmail.com>
2022-04-28 22:13:04 +0200

    add last layouts and PopularMoviePagedListAdapter class. Fill MainActivity and activity_main

---
+++ app/src/main/java/com/yerayyas/mvvm/ui/popular_movie/PopularMoviePagedListAdapter.kt
@@ -0,0 +1,152 @@
+package com.yerayyas.mvvm.ui.popular_movie
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.paging.PagedListAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.yerayyas.mvvm.data.api.POSTER_BASE_URL
+import com.yerayyas.mvvm.data.repository.NetworkState
+import com.yerayyas.mvvm.data.vo.Movie
+import com.yerayyas.mvvm.ui.single_movie_details.SingleActivity
+import kotlinx.android.synthetic.main.movie_list_item.view.*
+import kotlinx.android.synthetic.main.network_state_item.view.*
+import com.yerayyas.mvvm.R
+
+class PopularMoviePagedListAdapter(private val context: Context) :
+    PagedListAdapter<Movie, RecyclerView.ViewHolder>(MovieDiffCallback()) {
+    val movieViewType = 1
+    private val networkViewType = 2
+
+    private var networkState: NetworkState? = null
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view: View
+
+        return if (viewType == movieViewType) {
+            view = layoutInflater.inflate(R.layout.movie_list_item, parent, false)
+            MovieItemViewHolder(view)
+        } else {
+            view = layoutInflater.inflate(R.layout.network_state_item, parent, false)
+            NetworkStateItemViewHolder(view)
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (getItemViewType(position) == movieViewType) {
+            (holder as MovieItemViewHolder).bind(getItem(position), context)
+        } else {
+            (holder as NetworkStateItemViewHolder).bind(networkState)
+        }
+    }
+
+
+    private fun hasExtraRow(): Boolean {
+        return this.networkState != null && networkState != NetworkState.LOADED
+    }
+
+    override fun getItemCount(): Int {
+        return super.getItemCount() + if (hasExtraRow()) 1 else 0
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (hasExtraRow() && position == itemCount - 1) {
+            networkViewType
+        } else {
+            movieViewType
+        }
+    }
+
+
+    class MovieDiffCallback : DiffUtil.ItemCallback<Movie>() {
+        override fun areItemsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+
+    class MovieItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        @SuppressLint("SetTextI18n")
+        fun bind(movie: Movie?, context: Context) {
+            itemView.cv_movie_title.text = movie?.title
+
+            //Short description of the movie's overview
+            var endOfPhrase = ""
+            if(movie?.overview?.contains(". ") == true)
+            {
+                endOfPhrase = ".."
+            }
+
+            itemView.description.text =
+                movie?.overview?.substring(0, movie.overview.indexOf('.') + 1) + endOfPhrase
+
+            val moviePosterURL = POSTER_BASE_URL + movie?.posterPath
+            Glide.with(itemView.context)
+                .load(moviePosterURL)
+                .into(itemView.cv_iv_movie_poster)
+
+            itemView.setOnClickListener {
+                val intent = Intent(context, SingleActivity::class.java)
+                intent.putExtra("id", movie?.id)
+                context.startActivity(intent)
+            }
+
+        }
+
+    }
+
+    class NetworkStateItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        fun bind(networkState: NetworkState?) {
+            if (networkState != null && networkState == NetworkState.LOADING) {
+                itemView.progress_bar_item.visibility = View.VISIBLE
+            } else {
+                itemView.progress_bar_item.visibility = View.GONE
+            }
+
+            if (networkState != null && networkState == NetworkState.ERROR) {
+                itemView.error_msg_item.visibility = View.VISIBLE
+                itemView.error_msg_item.text = networkState.msg
+            } else if (networkState != null && networkState == NetworkState.ENDOFLIST) {
+                itemView.error_msg_item.visibility = View.VISIBLE
+                itemView.error_msg_item.text = networkState.msg
+            } else {
+                itemView.error_msg_item.visibility = View.GONE
+            }
+        }
+    }
+
+
+    fun setNetworkState(newNetworkState: NetworkState) {
+        val previousState = this.networkState
+        val hadExtraRow = hasExtraRow()
+        this.networkState = newNetworkState
+        val hasExtraRow = hasExtraRow()
+
+        if (hadExtraRow != hasExtraRow) {
+            if (hadExtraRow) {                             //hadExtraRow is true and hasExtraRow false
+                notifyItemRemoved(super.getItemCount())    //remove the progressbar at the end
+            } else {                                       //hasExtraRow is true and hadExtraRow false
+                notifyItemInserted(super.getItemCount())   //add the progressbar at the end
+            }
+        } else if (hasExtraRow && previousState != newNetworkState) { //hasExtraRow is true and hadExtraRow true and (NetworkState.ERROR or NetworkState.ENDOFLIST)
+            notifyItemChanged(itemCount - 1)       //add the network message at the end
+        }
+
+    }
+
+
+}
